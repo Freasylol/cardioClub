91ead93b3288ac833a4df159228f068a4904b895
Roman <>
2026-01-09 09:14:47 +0300

    fix: DescriptionText type fix

--- app/components/DescriptionText.tsx
+++ app/components/DescriptionText.tsx
@@ -1,10 +1,15 @@
 import styles from "../styles/DescriptionText.module.css";
 
+interface DescriptionTextProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
 export default function DescriptionText({
   children,
   className = "",
   ...props
-}) {
+}: DescriptionTextProps) {
   return (
     <p
       className={`${styles.text} ${className}`}
